2a5ef6628b66fb36a6da9e1e3a1f8dc80104f39b
mamaannie <99522502+mamaannie@users.noreply.github.com>
2022-02-12 17:14:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,13 +27,13 @@ def index():
     if request.method == "POST":
         rates = request.form.get("rates")
         print(rates)
-        model = joblib.load ("DBS")
-        pred = model.predict ([[float(rates)]])
+        model = joblib.load("DBS")
+        pred = model.predict([[float(rates)]])
         print(pred)
-        s ="The predicted DBS share price is" + str(pred [0][0])
-        return (render_template("index.html", result = s))
+        s ="The predicted DBS share price is" + str(pred[0][0])
+        return(render_template("index.html", result = s))
     else:
-        return (render_template("index.html", result="2"))
+        return(render_template("index.html", result="2"))
 
 
 # In[ ]:
